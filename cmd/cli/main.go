7fb14901ba05223ab97b2890b4715e8f92fff557
filmstar <finecoding@gmail.com>
2023-11-30 00:33:43 +0000

    Added Note explaining repo structure

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,6 +10,10 @@ func main() {
 
 	// This is a quick test to see if the internal import is working
 	// oxocli package contains types.go which defines Grid as [9]byte
+
+	// Added a stringer function each for Grid, Turn and Game to assist printing
+	// Put them new file stringer.go in oxocli package.  Copied much of it from oxo2.go
+
 	b := oxocli.Grid([]byte("X........"))
 	fmt.Println("This is a board", b)
 	t := oxocli.Turn{
